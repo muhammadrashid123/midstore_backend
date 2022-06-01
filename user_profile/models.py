a6343020fac96bfc74a4694228e2dd12af8d4d1a
Nabiha Khan <nabihak154@gmail.com>
2022-06-01 13:37:16 +0500

    Cart api in progress

--- user_profile/models.py
+++ user_profile/models.py
@@ -4,6 +4,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.fields import EmailField
+from product_management.models import Products
 # Create your models here.
 from seller_management.models import Shop
 
@@ -67,3 +68,39 @@ class User(AbstractUser):
     # Use EMAIL insted of USERNAME
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['username']
+
+
+# Cart model goes here
+class Cart(models.Model):
+    user = models.ForeignKey(User,to_field="uuid", on_delete=models.SET_NULL, null=True, blank=True)
+    item = models.ForeignKey(Products, on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.IntegerField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} - {} - {} - {} - {}".format(self.user,
+                                               self.item,
+                                               self.quantity,
+                                               self.created_at,
+                                               self.updated_at)
+
+# delivery cost
+class DeliveryCost(models.Model):
+    status = models.CharField(max_length=7,
+                              choices=(('Active', 'active'), ('Passive', 'passive')),
+                              default="passive",
+                              null=False)
+    cost_per_delivery = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    cost_per_product = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    fixed_cost = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} - {} - {} - {} - {} - {}".format(self.status,
+                                                    self.cost_per_delivery,
+                                                    self.cost_per_product,
+                                                    self.fixed_cost,
+                                                    self.created_at,
+                                                    self.updated_at)
